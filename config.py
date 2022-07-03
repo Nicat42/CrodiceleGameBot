1ca91a47a9939167dcf9213c93836946e7da0787
Kenan20222 <104547432+Kenan20222@users.noreply.github.com>
2022-07-03 04:14:20 +0400

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ load_dotenv()
 que = {}
 admins = {}
 
-BOT_TOKEN = getenv("BOT_TOKEN", "5311094936:AAHbpGSeys8BjQ5uvm4gCGBhjfHYkWqWGWk")
+BOT_TOKEN = getenv("BOT_TOKEN", "5537646590:AAEwqO5MazvP_Kfmaf-bLdunbslxpO8s7KY")
 BOT_NAME = getenv("BOT_NAME", "CrocadilBot") 
-API_ID = int(getenv("API_ID" 11742375))
-API_HASH = getenv("API_HASH", "0c37f648f2fea4cd39b35579eb7a2f64")
+API_ID = int(getenv("API_ID" 13544181))
+API_HASH = getenv("API_HASH", "1cf5e591506286e82e89e98b2436ebb6")
 BOT_USERNAME = getenv("BOT_USERNAME", "Corcadildsidhlebot")
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://nusrte:Nusret.2006@cluster0.r8ckx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://kenanbitcoin:PunnUo7gN6XpNidk@cluster0.gtmaj.mongodb.net/Cluster0?retryWrites=true&w=majority")
