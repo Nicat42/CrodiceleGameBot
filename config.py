0ccc802eb3d0675bf346ff301ad80f0154cf5d96
Vusaldi <93197397+Vusaldi@users.noreply.github.com>
2022-12-20 21:26:31 +0400

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ load_dotenv()
 que = {}
 admins = {}
 
-BOT_TOKEN = getenv("BOT_TOKEN", "5537646590:AAEwqO5MazvP_Kfmaf-bLdunbslxpO8s7KY")
-BOT_NAME = getenv("BOT_NAME", "CrocadilBot") 
+BOT_TOKEN = getenv("BOT_TOKEN", "5631477617:AAFrf8wg07Anl5MpmZj3LMPCEZkJw966hCM")
+BOT_NAME = getenv("BOT_NAME", "Wieuehebot") 
 API_ID = int(getenv("API_ID" 13544181))
 API_HASH = getenv("API_HASH", "1cf5e591506286e82e89e98b2436ebb6")
-BOT_USERNAME = getenv("BOT_USERNAME", "Corcadildsidhlebot")
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://kenanbitcoin:PunnUo7gN6XpNidk@cluster0.gtmaj.mongodb.net/Cluster0?retryWrites=true&w=majority")
+BOT_USERNAME = getenv("BOT_USERNAME", "Wieuehebot")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Kolgeli:sesiz@cluster0.sgubr8z.mongodb.net/Elmdar?retryWrites=true&w=majority")
