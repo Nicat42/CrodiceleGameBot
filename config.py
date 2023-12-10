b4e59f7ed8ce6dfa8cacddbebbf759dfa84b302a
ElikoAndMee <legendmuzkao99@gmail.com>
2023-12-10 13:58:51 +0400

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ admins = {}
 BOT_TOKEN = getenv("BOT_TOKEN", "6648598150:AAFGZvnVU1pOQlmDDQ-OHUI_KI2NCi0cmBM")
 BOT_NAME = getenv("BOT_NAME", "GameYoxlamaBot") 
 API_ID = int(getenv("API_ID" 29171757))
-API_HASH = getenv("API_HASH", ""f2a2d9619b545d116fc42b5fee5d592e")
+API_HASH = getenv("API_HASH", "f2a2d9619b545d116fc42b5fee5d592e")
 BOT_USERNAME = getenv("BOT_USERNAME", "GameYoxlamaBot")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Kolgeli:sesiz@cluster0.sgubr8z.mongodb.net/Elmdar?retryWrites=true&w=majority")
